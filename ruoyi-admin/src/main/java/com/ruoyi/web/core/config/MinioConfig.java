42884adf2db3477d90f24fa2e8038e28f504ac2d
Mashir0 <mashir0@shuanglintech.com>
2024-07-31 18:18:29 +0800

    aaa

--- ruoyi-admin/src/main/java/com/ruoyi/web/core/config/MinioConfig.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/core/config/MinioConfig.java
@@ -9,36 +9,11 @@ import org.springframework.context.annotation.Configuration;
 @Data
 @Configuration
 public class MinioConfig {
-
-    /**
-     * 访问地址
-     */
-    @Value("${minio.endpoint}")
-    private String endpoint;
-
-    /**
-     * accessKey类似于用户ID，用于唯一标识你的账户
-     */
-    @Value("${minio.accessKey}")
-    private String accessKey;
-
-    /**
-     * secretKey是你账户的密码
-     */
-    @Value("${minio.secretKey}")
-    private String secretKey;
-
-    /**
-     * 默认存储桶
-     */
-    @Value("${minio.bucketName}")
-    private String bucketName;
-
     @Bean
     public MinioClient minioClient() {
         MinioClient minioClient = MinioClient.builder()
-                .endpoint(endpoint)
-                .credentials(accessKey, secretKey)
+                .endpoint("http://192.168.1.204:9001")
+                .credentials("monio_root", "monio_123456")
                 .build();
         return minioClient;
     }
